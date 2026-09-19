b39aadd13ab7afad16d6ab8366624609af7c265f
klgaby440-lang <klgaby440@gmail.com>
2026-09-19 17:21:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -468,6 +468,8 @@ with engine.connect() as conn:
     conn.execute(text("ALTER TABLE teachers ADD COLUMN IF NOT EXISTS subject VARCHAR;"))
     conn.execute(text("ALTER TABLE teachers ADD COLUMN IF NOT EXISTS status VARCHAR DEFAULT 'Actif';"))
     conn.execute(text("ALTER TABLE teachers ADD COLUMN IF NOT EXISTS teacher_code VARCHAR(50);"))
+    conn.execute(text("ALTER TABLE school_information ADD COLUMN IF NOT EXISTS email TEXT;"))
+    school_information
     conn.commit()
 
 # ---------------------------------------------------------
